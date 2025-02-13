de336ac4362b0ca754851541a2242e55f09a75f1
NTeditor <nteditor4@gmail.com>
2025-02-13 22:08:04 +0300

    init

---
+++ customize.sh
@@ -0,0 +1,11 @@
+chmod 644 $MODPATH/system/etc/default-permissions/microg-permissions.xml
+chmod 644 $MODPATH/system/etc/permissions/com.google.android.maps.xml
+chmod 644 $MODPATH/system/etc/permissions/privapp-permissions-microg.xml
+chmod 644 $MODPATH/system/etc/sysconfig/microg.xml
+chmod 644 $MODPATH/system/app/ServicesFrameworkProxy/ServicesFrameworkProxy.apk
+chmod 644 $MODPATH/system/priv-app/Droid-ify/Droid-ify.apk
+chmod 644 $MODPATH/system/priv-app/microGCompanion/microGCompanion.apk
+chmod 644 $MODPATH/system/priv-app/microGServices/microGServices.apk
+chmod 644 $MODPATH/system/product/overlay/microGOverlay/microGOverlay.apk
+chmod 644 $MODPATH/system/product/overlay/PlayStoreOverlay/PlayStoreOverlay.apk
+chmod 644 $MODPATH/system/framework/com.google.android.maps.jar
