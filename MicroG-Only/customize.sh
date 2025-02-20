de7010873bae5ddaf1beda4ddfbccee3d6360ace
NTeditor <nteditor4@gmail.com>
2025-02-20 03:09:13 +0300

    init

--- MicroG-Only/customize.sh
+++ MicroG-Only/customize.sh
@@ -1,3 +1,19 @@
+# product or system?
+
+if [ $API -ge 28 ]; then
+	echo "$API"
+	echo "1"
+elif [ $API -lt 28 ]; then
+	echo "$API"
+	echo "0"
+else
+	echo "$API"
+	echo "Unknown"
+fi
+
+# product or system? end
+
+
 ui_print "- Установка разрешений..."
 
 set_perm $MODPATH/system/etc/default-permissions/microg-permissions.xml 0 0 0644
